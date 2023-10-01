2a0736087adddd86497acac27d41bd31d4639ba8
FestusMike <festusmike98@gmail.com>
2023-10-01 22:16:31 +0100

    Changes.....

--- package/__init__.py
+++ package/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:Timi1234@localhost/hngx_task'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app, supports_credentials=True)  # Enable CORS with credentials support
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://{username}:{password}@{hostname}/{databasename}".format(
+    username="Timi1234",
+    password="",
+    hostname="Timi1234.mysql.pythonanywhere-services.com",
+    databasename="Timi1234$database_name"
+)
+app.config['DEBUG'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 app.config['SECRET_KEY'] = '02ff7dc692594b26fedb715e'
 
 db = SQLAlchemy(app)
--- package/routes.py
+++ package/routes.py
@@ -1,11 +1,13 @@
-from flask import request, redirect, render_template, url_for, send_from_directory
+from flask import request, redirect, render_template, url_for, send_from_directory, jsonify
 from package import db, app
 from package.models import Video
 import os
 import datetime
+from werkzeug.exceptions import BadRequestKeyError
+
 
 # Set the upload folder
-UPLOAD_FOLDER = 'C:\\Users\\Micheal\\ChromeExtensionHNG\\package\\static\\uploads'
+UPLOAD_FOLDER = '/home/Myusername/mysite/package/static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 250 * 1024 * 1024  # 250MB limit
 
@@ -61,3 +63,59 @@ def play_video(video_id):
 def send_video(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+# api routes
+
+@app.route('/api/upload', methods=['POST'])
+def api_upload_video():
+    try:
+        file = request.files['video']
+    except BadRequestKeyError:
+        return jsonify({'error': "No file part in the request with the key 'video'"}), 400
+
+    # Check file size
+    file_size = int(request.headers['Content-Length'])
+    if file_size > app.config['MAX_CONTENT_LENGTH']:
+        return jsonify({'error': 'File size exceeds the limit of 250MB'}), 400
+
+    video_name = file.filename
+    date_uploaded = datetime.datetime.now()
+
+    # Check if the file already exists on disk
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], video_name)
+    if os.path.exists(file_path):
+        return jsonify({'error': 'File already exists'}), 400
+
+    # Check if the file already exists in the database
+    existing_video = Video.query.filter_by(video_name=video_name).first()
+    if existing_video:
+        return jsonify({'error': 'File already exists in the database'}), 400
+
+    # Save video to disk
+    file.save(file_path)
+
+    # Save video information to the database
+    video = Video(video_name=video_name, date_uploaded=date_uploaded)
+    db.session.add(video)
+    db.session.commit()
+
+    # Return success response
+    return jsonify({'message': 'Video uploaded successfully', 'video_id': video.id}), 201
+
+@app.route('/api/videos', methods=['GET'])
+def api_list_videos():
+    videos = Video.query.all()
+    video_data = [{'id': video.id, 'video_name': video.video_name, 'date_uploaded': video.date_uploaded,
+                   'video_url': url_for('send_video', filename=video.video_name)} for video in videos]
+    return jsonify(video_data)
+
+@app.route('/api/videos/<int:id>', methods=['GET'])
+def get_video_by_id(id):
+    video = Video.query.filter_by(id=id).first()
+    if not video:
+        return jsonify({"message" : "Video doesn't exist on disk"}), 400
+    else:
+        video_data = {"id" : video.id, "video_name" : video.video_name, 
+        "date_uploaded" : video.date_uploaded,
+        "video_url" : url_for('send_video', filename=video.video_name)
+        }
+        return jsonify(video_data) 
