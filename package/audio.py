b028be38f6bc993668e7f1c08dfb62757b5fa8e4
FestusMike <festusmike98@gmail.com>
2023-10-01 13:57:30 +0100

    deleted audio.py

--- package/audio.py
+++
@@ -1,5 +0,0 @@
-# import whisper
-
-# model = whisper.load_model("base")
-# result = model.transcribe("C:\\Users\\Micheal\\ChromeExtensionHNG\\package\\static\\uploads\\drake.mkv")
-# # print(result["text"])
